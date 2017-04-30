e5cdcd94a5a0f5d54280ca7b1a4bcefef336713a
Oleg <Oleg@OLEG->
2017-04-30 15:46:08 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpContextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(376, 162);
+            this.dataGridView1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.dataGridView1, "Таблица для вывода сотрудников");
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseMove);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 285);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(376, 167);
+            this.dataGridView2.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.dataGridView2, "Таблица для вывода предметов");
+            this.dataGridView2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView2_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Загрузить данные";
+            this.toolTip1.SetToolTip(this.button1, "Кнопка для загрузки данных");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(799, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.ToolTipText = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.helpContextToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.ToolTipText = "Меню подсказок";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.ToolTipText = "Окно \"О программе\"";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // helpContextToolStripMenuItem
+            // 
+            this.helpContextToolStripMenuItem.Name = "helpContextToolStripMenuItem";
+            this.helpContextToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.helpContextToolStripMenuItem.Text = "Help context";
+            this.helpContextToolStripMenuItem.ToolTipText = "Вызов справки о программе";
+            this.helpContextToolStripMenuItem.Click += new System.EventHandler(this.helpContextToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(154, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Предметы";
+            this.toolTip1.SetToolTip(this.label1, "Таблица для вывода предметов");
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(394, 80);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(371, 162);
+            this.dataGridView3.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.dataGridView3, "Таблица для вывода заказов");
+            this.dataGridView3.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellClick);
+            this.dataGridView3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView3_MouseMove);
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(397, 285);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(368, 167);
+            this.dataGridView4.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.dataGridView4, "Таблица для вывода деталей");
+            this.dataGridView4.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView4_CellClick);
+            this.dataGridView4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView4_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(535, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Заказы";
+            this.toolTip1.SetToolTip(this.label2, "Таблица для вывода заказов");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(535, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Детали";
+            this.toolTip1.SetToolTip(this.label3, "Таблица для вывода деталей");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(141, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Сотрудники";
+            this.toolTip1.SetToolTip(this.label4, "Таблица для вывода сотрудников");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 485);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Информационный блок";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 507);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpContextToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }        
+        public XmlReader xmlEmployees;
+        public XmlReader xmlItems;
+        public XmlReader xmlOrders;
+        public XmlReader xmlParts;
+
+        public DataSet dsEmployees;
+        public DataSet dsItems;
+        public DataSet dsOrders;
+        public DataSet dsParts;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                xmlEmployees = XmlReader.Create("employee.xml", new XmlReaderSettings());
+                //xmlClients = XmlReader.("client.xml", new XmlReaderSettings());
+                //dgsdgds
+                //xmlClients = XmlReader.Create(new StringReader(GET("http://localhost/docxml.php?file=client")));
+
+                dsEmployees = new DataSet();
+                dsEmployees.ReadXml(xmlEmployees);
+                dataGridView1.DataSource = dsEmployees.Tables[0];
+
+                string[] namecol = new string[6] { "№ сотрудника", "Фамилия", "Имя", "PhoneExt", "Дата найма", "Зарплата" };
+                                
+                for (int i= 0; i < 6; i++)     dataGridView1.Columns[i].HeaderText = namecol[i];
+                items();
+                orders();
+                parts();           
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form about = new About();
+            about.ShowDialog();
+        }
+
+        
+        private void filt1(int numcol, string namecol)
+        {
+            string act = dataGridView1.CurrentRow.Cells[numcol].Value.ToString();
+            DataTable firstTable = dsOrders.Tables[0];
+            DataView view1 = new DataView(firstTable);
+            BindingSource bs = new BindingSource();
+            bs.DataSource = view1;
+            bs.Filter = String.Format(namecol+" LIKE '{0}'", act);
+            dataGridView3.DataSource = bs;
+
+        }
+        private void filt2(int numcol, string namecol)
+        {
+            string act = dataGridView3.CurrentRow.Cells[numcol].Value.ToString();
+            DataTable firstTable = dsItems.Tables[0];
+            DataView view1 = new DataView(firstTable);
+            BindingSource bs = new BindingSource();
+            bs.DataSource = view1;
+            bs.Filter = String.Format(namecol + " LIKE '{0}'", act);
+            dataGridView2.DataSource = bs;
+
+        }
+        private void filt3(int numcol, string namecol)
+        {
+            string act = dataGridView4.CurrentRow.Cells[numcol].Value.ToString();
+            DataTable firstTable = dsItems.Tables[0];
+            DataView view1 = new DataView(firstTable);
+            BindingSource bs = new BindingSource();
+            bs.DataSource = view1;
+            bs.Filter = String.Format(namecol + " LIKE '{0}'", act);
+            dataGridView2.DataSource = bs;
+
+        }
+        private void items()
+            {
+            try
+            {
+                xmlItems = XmlReader.Create(new StringReader(GET("http://localhost/docxml.php?file=items")));
+                dsItems = new DataSet();
+                dsItems.ReadXml(xmlItems);
+                dataGridView2.DataSource = dsItems.Tables[0];
+                string[] namecol = new string[5] { "№ заказа", "№ предмета", "№ детали", "Qty", "Скидка" };
+                for (int i = 0; i < 5; i++) dataGridView2.Columns[i].HeaderText = namecol[i];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private string GET(string Url)
+        {
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(Url);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            string output ="";
+            using (StreamReader stream = new StreamReader(
+                 resp.GetResponseStream(), Encoding.UTF8))
+            {
+                output+=stream.ReadToEnd();
+            }
+            return output;
+        }
+
+        private void orders (){
+            try
+            {
+                xmlOrders = XmlReader.Create(new StringReader(GET("http://localhost/docxml.php?file=orders")));
+                dsOrders = new DataSet();
+                dsOrders.ReadXml(xmlOrders);
+                dataGridView3.DataSource = dsOrders.Tables[0];
+                string[] namecol = new string[5] { "№ заказа", "№ клиента", "Дата продажи", "Дата доставки", "№ рабочего" };
+                for (int i = 0; i < 5; i++) dataGridView3.Columns[i].HeaderText = namecol[i];
+            }
+            catch (Exception ex)
+            { }
+        }
+        private void parts()
+        {
+            try
+            {
+                xmlParts = XmlReader.Create(new StringReader(GET("http://localhost/docxml.php?file=parts")));
+                dsParts = new DataSet();
+                dsParts.ReadXml(xmlParts);
+                dataGridView4.DataSource = dsParts.Tables[0];
+                string[] namecol = new string[7] { "№ детали", "№ поставщика", "Описание", "В наличии", "На заказ", "Стоимость", "Список Цен" };
+                for (int i = 0; i < 7; i++) dataGridView4.Columns[i].HeaderText = namecol[i];
+            }
+            catch (Exception ex)
+            { }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void helpContextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HelpNavigator navigator = HelpNavigator.TopicId;
+            Help.ShowHelp (this, @"C:\Users\Кирилл\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\bin\Debug\help.chm", navigator);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            filt1(0,"EmpNo");
+        }
+
+        private void dataGridView3_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            filt2(0, "OrderNo");
+        }
+
+        private void dataGridView4_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            filt3(0, "PartNo");
+        }
+
+        private void dataGridView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label5.Text = "Таблица для отображения сотрудников";
+        }
+
+        private void dataGridView3_MouseMove(object sender, MouseEventArgs e)
+        {
+            label5.Text = "Таблица для отображения заказов";
+        }
+
+        private void dataGridView2_MouseMove(object sender, MouseEventArgs e)
+        {
+            label5.Text = "Таблица для отображения предметов";
+        }
+
+        private void dataGridView4_MouseMove(object sender, MouseEventArgs e)
+        {
+            label5.Text = "Таблица для отображения деталей";
+        }
+    }
+}
